c395dad10e4f4e88c62a618ff67809aab8a61528
Harsha-reddyyy <harshavardhanreddy.0712@gmailcom>
2026-02-24 15:49:02 +0530

    login button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">StuBite</div>
+      <div className="logo"> 🍔 StuBite</div>
 
       <ul className="nav-links">
         <li>Home</li>
         <li>Menu</li>
         <li>Orders</li>
-        <li>Login</li>
       </ul>
+      <button className="login-btn">Login</button>
     </nav>
   );
 }
